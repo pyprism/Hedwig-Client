d0fd206e4a36571d881b95f9d26f0759151e1dca
Ashutosh Das <git.pyprism@gmail.com>
2026-06-29 13:09:35 +0600

    feat: added message density controller for dynamic thread list density settings

---
+++ lib/features/settings/presentation/controllers/density_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'density_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$messageDensityHash() => r'';
+
+/// See also [MessageDensity].
+@ProviderFor(MessageDensity)
+final messageDensityProvider =
+    NotifierProvider<MessageDensity, String>.internal(
+      MessageDensity.new,
+      name: r'messageDensityProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$messageDensityHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$MessageDensity = Notifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
