969cec4ca06f54ec114b3c93e0328c90701f2e8a
Ashutosh Das <git.pyprism@gmail.com>
2026-06-29 14:06:59 +0600

    refactor: changed Contact class to abstract for improved extensibility

--- lib/shared/models/contact.dart
+++ lib/shared/models/contact.dart
@@ -4,7 +4,7 @@ part 'contact.freezed.dart';
 part 'contact.g.dart';
 
 @freezed
-class Contact with _$Contact {
+abstract class Contact with _$Contact {
   const factory Contact({
     required String id,
     @JsonKey(name: 'mailbox') required String mailboxId,
