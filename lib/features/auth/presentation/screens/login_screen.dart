647bd5d17e6fa3ca227f0161c79c25bd6f952b5b
Ashutosh Das <git.pyprism@gmail.com>
2026-09-06 16:03:21 +0600

    feat(auth): initialized server controller with app config or default URL

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hedwig_client/core/api/error_interceptor.dart';
+import 'package:hedwig_client/core/config/app_config.dart';
 import 'package:hedwig_client/core/config/env_config.dart';
 import 'package:hedwig_client/core/error/failure.dart';
 import 'package:hedwig_client/features/auth/presentation/controllers/auth_controller.dart';
@@ -15,11 +16,19 @@ class LoginScreen extends ConsumerStatefulWidget {
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _serverCtrl = TextEditingController(text: EnvConfig.defaultApiUrl);
+  late final TextEditingController _serverCtrl;
   final _usernameCtrl = TextEditingController();
   final _passwordCtrl = TextEditingController();
   bool _obscure = true;
   String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _serverCtrl = TextEditingController(
+      text: ref.read(appConfigProvider) ?? EnvConfig.defaultApiUrl,
+    );
+  }
 
   @override
   void dispose() {
